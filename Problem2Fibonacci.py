a4d95ecacdcdf78f976d70dc83d125ff717cfb4f
lolJona <JonathanDotson@gmail.com>
2022-08-31 06:56:15 -0700

    Update Problem2Fibonacci.py

--- Problem2Fibonacci.py
+++ Problem2Fibonacci.py
@@ -1,3 +1,12 @@
+"""
+I'm sure I can do this with a while loop, 
+along with adding something along the lines of fiblist[-1] and fiblist[-2]
+and evaluating and appending if the resultant value evaluates to < 4000000
+but I can't figure out how to implement it.  Thoughts?
+
+This gives the correct answer but is far from elegant or efficiently written.
+"""
+
 fibsum = 0
 fiblist = [1, 2]
 num1 = 1
