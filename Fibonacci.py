bb5af254f4a9e1c9a942e2a30a166a075d0152f5
lolJona <JonathanDotson@gmail.com>
2022-08-31 06:44:50 -0700

    Create Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,101 @@
+fibsum = 0
+fiblist = [1, 2]
+num1 = 1
+num2 = 2
+val1 = num1 + num2
+fiblist.append(val1)
+print(val1)
+num1 = val1
+print(num1)
+val2 = num1 + num2
+fiblist.append(val2)
+num2 = val2
+val1 = num1 + num2
+fiblist.append(val1)
+num1 = val1
+val2 = num1 + num2
+fiblist.append(val2)
+num2 = val2
+val1 = num1 + num2
+fiblist.append(val1)
+num1 = val1
+val2 = num1 + num2
+fiblist.append(val2)
+num2 = val2
+val1 = num1 + num2
+fiblist.append(val1)
+num1 = val1
+val2 = num1 + num2
+fiblist.append(val2)
+num2 = val2
+val1 = num1 + num2
+fiblist.append(val1)
+num1 = val1
+val2 = num1 + num2
+fiblist.append(val2)
+num2 = val2
+val1 = num1 + num2
+fiblist.append(val1)
+num1 = val1
+val2 = num1 + num2
+fiblist.append(val2)
+num2 = val2
+val1 = num1 + num2
+fiblist.append(val1)
+num1 = val1
+val2 = num1 + num2
+fiblist.append(val2)
+num2 = val2
+val1 = num1 + num2
+fiblist.append(val1)
+num1 = val1
+val2 = num1 + num2
+fiblist.append(val2)
+num2 = val2
+val1 = num1 + num2
+fiblist.append(val1)
+num1 = val1
+val2 = num1 + num2
+fiblist.append(val2)
+num2 = val2
+val1 = num1 + num2
+fiblist.append(val1)
+num1 = val1
+val2 = num1 + num2
+fiblist.append(val2)
+num2 = val2
+val1 = num1 + num2
+fiblist.append(val1)
+num1 = val1
+val2 = num1 + num2
+fiblist.append(val2)
+num2 = val2
+val1 = num1 + num2
+fiblist.append(val1)
+num1 = val1
+val2 = num1 + num2
+fiblist.append(val2)
+num2 = val2
+val1 = num1 + num2
+fiblist.append(val1)
+num1 = val1
+val2 = num1 + num2
+fiblist.append(val2)
+num2 = val2
+val1 = num1 + num2
+fiblist.append(val1)
+num1 = val1
+val2 = num1 + num2
+fiblist.append(val2)
+num2 = val2
+val1 = num1 + num2
+fiblist.append(val1)
+num1 = val1
+val2 = num1 + num2
+fiblist.append(val2)
+num2 = val2
+print(fiblist)
+for fib in fiblist:
+    if fib % 2 == 0:
+        fibsum += fib
+print(fibsum)
