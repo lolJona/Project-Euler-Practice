568933455a9adc4c2bec6e75419a5a7a0af8960e
lolJona <JonathanDotson@gmail.com>
2022-09-19 21:09:44 -0700

    Update Problem9Squares.py

--- Problem9Squares.py
+++ Problem9Squares.py
@@ -1,5 +1,5 @@
 for a in range(1, 1000):
 	for b in range(1, 1000):
 		for c in range(1, 1000):
-			if (a**2 + b**2 == c**2) and (a + b + c == 1000):
+			if (a < b < c) and (a**2 + b**2 == c**2) and (a + b + c == 1000):
 				print("The value of a is " + str(a) + ", the value of b is " + str(b) + ", and the value of c is " + str(c) + ".")
