efabab6fbc07219aad7913b5b1c24f7077553e03
lolJona <JonathanDotson@gmail.com>
2022-09-26 23:09:24 -0700

    Update Project39IntRightTriangles.py

--- Project39IntRightTriangles.py
+++ Project39IntRightTriangles.py
@@ -2,8 +2,8 @@
 
 import statistics
 plist = []
-for a in range(1, 1000):
-    for b in range(1, 1000):
+for a in range(1, 501):
+    for b in range(1, 501):
         for c in range(1, 1000):
             if (a**2 + b**2 == c**2) and (a + b + c <= 1000):
                 p = str(a + b + c)
